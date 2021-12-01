65ef4fc8fd0e2fb58ecf0efe9315b0cc3f307351
chameleoneyes <khambikovar@gmail.com>
2021-12-01 22:01:14 +0300

    25 Задание. Тест логин в Мантис

---
+++ Fixture/application.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from Fixture.session import SessionHelper
+
+
+class Application:
+
+    def __init__(self, browser, url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+# Edge doesn't work
+        elif browser == "ie":
+            self.wd = webdriver.Edge()
+        else:
+            raise ValueError("Unrecognized Browser %s" % browser)
+#        self.wd.implicitly_wait(10)
+        self.session = SessionHelper(self)
+        self.url = url
+
+    def open_home_page(self):
+        wd = self.wd
+        if wd.current_url != self.url:
+            wd.get(self.url)
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def clear_fixture(self):
+        self.wd.quit()
---
+++ conftest.py
@@ -0,0 +1,78 @@
+import json
+import pytest
+from Fixture.application import Application
+#from Fixture.db import DBfixture
+import os.path
+import importlib
+
+
+
+fixture = None
+target = None
+
+
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as cf:
+            target = json.load(cf)
+    return target
+
+
+@pytest.fixture
+def app(request):
+    global fixture
+    browser = request.config.getoption("--browser")
+    web_config = load_config(request.config.getoption("--target"))['web']
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, url=web_config["testUrl"])
+    return fixture
+
+
+@pytest.fixture(scope='session', autouse=True)
+def stop(request):
+    def f_exit():
+        fixture.clear_fixture()
+    request.addfinalizer(f_exit)
+    return fixture
+
+
+@pytest.fixture(scope='session')
+def db(request):
+    db_config = load_config(request.config.getoption("--target"))['db']
+    dbfixture = DBfixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], pwd=db_config['password'])
+    def fin():
+        dbfixture.destroy()
+    request.addfinalizer(fin)
+    return dbfixture
+
+
+@pytest.fixture
+def check_ui(request):
+    return request.config.getoption("--check_ui")
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--target", action="store", default="target.json")
+    parser.addoption("--check_ui", action="store_true")
+
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            tdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, tdata, ids=[str(x) for x in tdata])
+        elif fixture.startswith("json_"):
+            tdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, tdata, ids=[str(x) for x in tdata])
+
+
+def load_from_module(module):
+    return importlib.import_module("data.%s" % module).tdata
+
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
